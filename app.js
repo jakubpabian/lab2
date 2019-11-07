89e36da2c7e10d54b6be599e0c6f2f5f55631bc0
jakubpabian <56395128+jakubpabian@users.noreply.github.com>
2019-11-07 13:27:06 +0100

    Update app.js
    commit

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const _= require('lodash');
 let mailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 // importujemy biblioteke lodash nastepnie tworzymy stałą mail regex do filtrowania adresów mail
+
+
+const getMax = array =>{
+    return _.max(array);
+}
+ 
+const getMin = array =>{
+    return _.min(array);
+}
+console.log(getMax([1,2,3,4]));
+console.log(getMin([1,2,3]));
+ 
